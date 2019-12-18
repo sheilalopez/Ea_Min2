dc4cc99aa9693d85c1f5f4f9052ec0af7ff8359c
sheilalopez <47568080+sheilalopez@users.noreply.github.com>
2019-12-18 15:01:43 +0100

    user service

---
+++ Ea_Min2/src/app/services/student.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Student} from "../models/student";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  urlStudent: string = 'http://192.168.1.38:3000/students';
+
+  constructor(private http: HttpClient) { }
+  //Rutas Url de backend.
+  getTelematic(): Observable<Student[]>{
+    return this.http.get<Student[]>(this.urlStudent + '/telematics');
+  }
+  getSystem(): Observable<Student[]>{
+    return this.http.get<Student[]>(this.urlStudent + '/systems');
+  }
+  getAeros(): Observable<Student[]>{
+    return this.http.get<Student[]>(this.urlStudent + '/aeros');
+  }
+}
