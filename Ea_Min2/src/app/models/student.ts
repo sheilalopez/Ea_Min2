839d0c0b3f7eb01f6f58a85d6935e16cf3eedb8e
sheilalopez <47568080+sheilalopez@users.noreply.github.com>
2019-12-18 14:49:25 +0100

    model user added

---
+++ Ea_Min2/src/app/models/student.ts
@@ -0,0 +1,15 @@
+export class Student {
+    name: string;
+    address: string;
+    phones: [
+        {
+            description: string;
+            number: string;
+        }
+    ];
+    studies: [
+        {
+            name: string;
+        }
+    ];
+}
